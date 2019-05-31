f106c80153eef8d4b547b112b4c6bec180436156
romansmolakov <smolyakovroma@gmail.com>
2019-05-31 18:22:03 +0700

    Initial Commit

---
+++ ChatClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatClient
+//
+//  Created by Роман Смоляков on 31/05/2019.
+//  Copyright © 2019 Роман Смоляков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
